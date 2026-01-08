051995d1bbaa5660096554efdda8e82d307b334b
tluthin67 <theo.luthin2903@gmail.com>
2026-01-08 14:56:40 +0100

    new main.go

--- main.go
+++ main.go
@@ -1,21 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "net/http"
+  "os"
+  "html/template"
+  "log"
 )
 
-func pageAccueil(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		http.ServeFile(w, r, "index.html")
-	} else {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+ if r.URL.Path != "/" {
+  errorHandler(w, r, http.StatusNotFound)
+  return
+ }
+
+ tmpl, err := template.ParseFiles("template/index.html")
+ if err != nil {
+  log.Println("Erreur template index.html:", err)
+  http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+  return
+ }
+
+ err = tmpl.Execute(w, nil)
+ if err != nil {
+  log.Println("Erreur exécution template index.html:", err)
+ }
 }
 
-func main() {
-	http.HandleFunc("/", pageAccueil)
 
-	fmt.Println("Le serveur est démarré sur le lien suivant : http://localhost:2918")
-	http.ListenAndServe(":2918", nil)
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+ w.WriteHeader(status)
+ tmpl, err := template.ParseFiles("template/error.html")
+ if err != nil {
+  log.Println("Erreur template error.html:", err)
+  http.Error(w, http.StatusText(status), status)
+  return
+ }
+
+ data := struct {
+  Status  int
+  Message string
+ }{
+  Status:  status,
+  Message: http.StatusText(status),
+ }
+
+ err = tmpl.Execute(w, data)
+ if err != nil {
+  log.Println("Erreur exécution template error.html:", err)
+ }
+}
+
+
+func main() {
+http.HandleFunc("/", homeHandler)
+
+fs := http.FileServer(http.Dir("static"))
+http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+port := "3000"
+if os.Getenv("PORT") != "" {
+   port = os.Getenv("PORT")
+}
+
+log.Println("Serveur démarré sur http://localhost:" + port)
+log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
