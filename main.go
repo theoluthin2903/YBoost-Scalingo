6984c959b0a954bb0964bec9710ec5dd284093c7
ltheo <theo.luthin@ynov.com>
2026-02-18 11:17:28 +0100

    modif liaison base de donnees avec go

--- main.go
+++ main.go
@@ -1,11 +1,53 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func parseURLtoDSN(urlStr string) (string, error) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return "", err
+	}
+	password, _ := u.User.Password()
+	host := u.Host
+	if !strings.Contains(host, "(") {
+		host = "tcp(" + host + ")"
+	}
+	return fmt.Sprintf("%s:%s@%s%s", u.User.Username(), password, host, u.Path), nil
+}
+
+func getMySQLUrl() (string, error) {
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl != "" {
+		return parseURLtoDSN(dbUrl)
+	}
+	return "", nil
+}
+
+func openDB() (*sql.DB, error) {
+	dbUrl, err := getMySQLUrl()
+	if dbUrl == "" {
+		return nil, fmt.Errorf("SCALINGO_MYSQL_URL environment variable not set")
+	}
+	db, err := sql.Open("mysql", dbUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %v", err)
+	}
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+	return db, nil
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
