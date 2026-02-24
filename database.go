6f77dab98d405d10c6a3a5a7a87048895474738f
ltheo <theo.luthin@ynov.com>
2026-02-25 00:05:47 +0100

    separations fichiers go

---
+++ database.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func openDB() (*sql.DB, error) {
+	dbUrl := os.Getenv("SCALINGO_MYSQL_URL")
+	if dbUrl == "" {
+		return nil, fmt.Errorf("SCALINGO_MYSQL_URL environment variable not set")
+	}
+
+	dsn, err := parseURLtoDSN(dbUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func parseURLtoDSN(urlStr string) (string, error) {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		return "", err
+	}
+	pass, _ := u.User.Password()
+	host := u.Host
+	if !strings.Contains(host, "(") {
+		host = "tcp(" + host + ")"
+	}
+	return fmt.Sprintf("%s:%s@%s%s", u.User.Username(), pass, host, u.Path), nil
+}
---
+++ handlers.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"database/sql"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	var todos []Todo
+	if db != nil {
+		rows, err := db.Query("SELECT id, title, done FROM tests ORDER BY id ASC")
+		if err == nil {
+			defer rows.Close()
+			for rows.Next() {
+				var t Todo
+				rows.Scan(&t.ID, &t.Title, &t.Done)
+				todos = append(todos, t)
+			}
+		}
+	}
+
+	tmpl, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		log.Printf("[ERREUR] Template: %v", err)
+		http.Error(w, "Erreur Template", http.StatusInternalServerError)
+		return
+	}
+	log.Printf("[INFO] Page index chargée avec %d tâches", len(todos))
+	tmpl.Execute(w, todos)
+}
+
+func addTodoHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := r.FormValue("title")
+	log.Printf("[DEBUG] Tentative d'ajout du titre %s", title)
+
+	if title == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	if db != nil {
+		_, err := db.Exec("INSERT INTO tests (title, done) VALUES (?, ?)", title, false)
+		if err != nil {
+			log.Printf("[ERREUR CRITIQUE SQL] : %v", err)
+			http.Error(w, "Erreur SQL : "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		log.Printf("[SUCCESS] Tâche ajoutée : %s", title)
+	} else {
+		log.Println("[ERREUR] DB est nil !")
+		http.Error(w, "Connexion DB perdue", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func deleteTodoHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	id := r.URL.Query().Get("id")
+	log.Printf("[DEBUG] Tentative de suppression de l'ID %s", id)
+
+	if id == "" {
+		log.Println("[ERREUR] ID manquant dans l'URL")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	_, err := db.Exec("DELETE FROM tests WHERE id = ?", id)
+
+	if err != nil {
+		log.Printf("[ERREUR SQL] Échec de la suppression pour l'ID %s : %v", id, err)
+		http.Error(w, "Erreur suppression : "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("[SUCCESS] Tâche %s supprimée", id)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func updateTodoHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	id := r.URL.Query().Get("id")
+	_, err := db.Exec("UPDATE tests SET done = NOT done WHERE id = ?", id)
+	if err == nil {
+		log.Printf("[SUCCESS] état tâche à l'ID %s modifié !", id)
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func getMySQLUrl() (string, error) {
+	dbUrl := os.Getenv("SCALINGO_MYSQL_URL")
+	if dbUrl != "" {
+		return parseURLtoDSN(dbUrl)
+	}
+	return "", nil
+}
